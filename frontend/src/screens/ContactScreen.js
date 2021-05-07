6d1a5cdc9c4e71e90adac56ec0ccdd488ee368de
kmadhav907 <kmadhav907@gmail.com>
2021-05-08 00:45:05 +0530

    UI Changes

--- frontend/src/screens/ContactScreen.js
+++ frontend/src/screens/ContactScreen.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import { motion } from 'framer-motion';
+import { Link } from 'react-router-dom';
 import React, { useState } from 'react';
 import { useHistory } from 'react-router';
 import Loader from '../components/Loader';
@@ -14,25 +15,20 @@ const ContactScreen = () => {
   const history = useHistory();
   const onSubmitHandler = (e) => {
     e.preventDefault();
+    setDispMessage('Thank you for Submitting the form');
     setLoading(true);
     axios
       .post('http://localhost:5000/api/messages', { email, name, message })
-      .then((response) => {
-        setDispMessage('Thank you for Submitting the form');
-      })
+      .then((response) => {})
       .catch((error) => {
         setError(error.message);
       });
     setTimeout(() => {
       setLoading(false);
-      error ? history.push('/') : history.push('/contacts');
+      error ? history.push('/contacts') : history.push('/');
     }, 1000);
   };
-  const resetForm = () => {
-    setEmail('');
-    setMessage('');
-    setName('');
-  };
+
   if (error) {
     return <h1>{error}</h1>;
   }
@@ -46,7 +42,6 @@ const ContactScreen = () => {
           <motion.div
             className='contactFormCard'
             whileHover={{ scale: 1.1, transition: { duration: 1 } }}
-            whileTap={{ scale: 0.9, transition: { duration: 1 } }}
           >
             <form className='contact-form' onSubmit={onSubmitHandler}>
               <div className='inputContainer' style={{ fontSize: '1.1em' }}>
@@ -82,6 +77,49 @@ const ContactScreen = () => {
                 SUBMIT
               </button>
             </form>
+            <div className='myDetails'>
+              <div className='myDetailsItem'>
+                Email &nbsp;
+                <i style={{ fontSize: '2.5vh' }} className='fas fa-at'></i>
+                :&nbsp;
+                <a href='mailto:kmadhav907@gmail.com'>kmadhav907@gmail.com</a>
+              </div>
+
+              <div className='myDetailsItem'>
+                Contact &nbsp;
+                <i
+                  style={{ fontSize: '2.5vh' }}
+                  className='far fa-address-book'
+                ></i>
+                :&nbsp; +91-8197795213
+              </div>
+              <div className='myDetailsItem'>
+                Github &nbsp;
+                <i
+                  style={{ fontSize: '2.5vh' }}
+                  className='fab fa-github-square'
+                ></i>
+                :&nbsp;
+                <a href='https://github.com/kmadhav907'>kmadhav907</a>
+              </div>
+              <div className='myDetailsItem'>
+                Twitter &nbsp;
+                <i style={{ fontSize: '2.5vh' }} className='fab fa-twitter'></i>
+                :&nbsp;
+                <a href='https://twitter.com/MadhavBhat19'>MadhavBhat19</a>
+              </div>
+              <div className='myDetailsItem'>
+                LinkedIn &nbsp;
+                <i
+                  style={{ fontSize: '2.5vh' }}
+                  className='fab fa-linkedin-in'
+                ></i>
+                :&nbsp;
+                <a href='https://www.linkedin.com/in/madhav-bhat-k-0042ab1a1/'>
+                  Madhav Bhat K
+                </a>
+              </div>
+            </div>
           </motion.div>
         </div>
       )}
