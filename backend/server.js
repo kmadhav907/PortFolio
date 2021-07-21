309c63fe35bc609cc54405daf8a1f56879a8613e
kmadhav907 <kmadhav907@gmail.com>
2021-07-21 20:12:11 +0530

    First production

--- backend/server.js
+++ backend/server.js
@@ -3,16 +3,27 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 import connectDB from './config/connection.js';
 import messageRoutes from './routes/messageRoutes.js';
+import path from 'path';
 dotenv.config();
 const app = express();
+const __dirname = path.resolve();
 app.use(cors());
 
 app.use(express.json());
 connectDB();
+
 app.use('/api/messages', messageRoutes);
-app.get('/', (req, res) => {
-  res.send('API is running....');
-});
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  );
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....');
+  });
+}
 
 app.listen(process.env.PORT || 5000, () => {
   console.log(`Server is running on ${process.env.PORT || 5000}`);
