089b065e42763a5f54b13a0b62644348e6a812fd
kmadhav907 <kmadhav907@gmail.com>
2021-07-21 21:38:19 +0530

    Production bug fix

--- backend/controllers/messageController.js
+++ backend/controllers/messageController.js
@@ -35,7 +35,7 @@ export const createMessage = asyncHandler(async (req, res) => {
       transporter.sendMail(mailOptions, (error, info) => {
         if (error) {
           res.status(400);
-          throw new Error('SMTP ERROR');
+          // throw new Error('SMTP ERROR');
         } else {
           console.log('Email sent:' + info.response);
         }
